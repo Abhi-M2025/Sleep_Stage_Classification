09841fa991edbf02c5016fc5e0351f2c6397c7d8
Abhi <maneesh@wisc.edu>
2025-11-29 18:36:38 -0600

    Added download.py
    download.py contains the code to actually download the edf data for each sample from its source, the Haaglanden Medisch Centrum sleep staging database.

---
+++ download.py
@@ -0,0 +1,28 @@
+import requests
+import os
+
+BASE = "https://physionet.org/files/hmc-sleep-staging/1.1/recordings"
+
+def download(url, output):
+    print("Downloading:", output)
+    r = requests.get(url, stream=True)
+    r.raise_for_status()
+    with open(output, "wb") as f:
+        for chunk in r.iter_content(8192):
+            f.write(chunk)
+
+os.makedirs("raw", exist_ok=True)
+
+for i in range(73, 101):
+    sn = f"SN{i:03d}"
+
+    # recording
+    url1 = f"{BASE}/{sn}.edf?download"
+    out1 = f"raw/{sn}.edf"
+
+    # scoring
+    url2 = f"{BASE}/{sn}_sleepscoring.edf?download"
+    out2 = f"raw/{sn}_sleepscoring.edf"
+
+    download(url1, out1)
+    download(url2, out2)
