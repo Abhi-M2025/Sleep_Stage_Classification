9274aa81f00de6422544a6547f33df1650cfc50b
Kryant Ye <cye52@wisc.edu>
2025-12-04 14:54:36 -0600

    Finish Experiments

--- Model_MLP/model_train.py
+++ Model_MLP/model_train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
-from model_def import SleepStageMLP  # your MLP class
+from model_def import SleepStageMLP_ReLU, SleepStageMLP_LeakyReLU, SleepStageMLP_Tanh  # your MLP class
 import glob
 import os
 
@@ -17,13 +17,15 @@ class SleepDataset(Dataset):
             self.Y.append(data["y"])
 
         # combine all subjects
+        print("start concatenating")
         self.X = np.concatenate(self.X, axis=0)
         self.Y = np.concatenate(self.Y, axis=0)
+        print("finish concatenating")
 
         # flatten for MLP: (num_epochs, channels * samples)
         num_epochs, num_channels, num_samples = self.X.shape
-        self.X = self.X.reshape(num_epochs, num_channels * num_samples)
-
+        self.X = self.X.reshape(num_epochs, num_channels, num_samples//16, 16).mean(-1)  # (N, 4, 480)
+        self.X = self.X.reshape(num_epochs, -1)
         # convert to tensors
         self.X = torch.tensor(self.X, dtype=torch.float32)
         self.Y = torch.tensor(self.Y, dtype=torch.long)
@@ -36,22 +38,24 @@ class SleepDataset(Dataset):
 
 if __name__ == "__main__":
     # Create dataset and loader
-    train_ds = SleepDataset("../processed")
+    train_ds = SleepDataset("../processed_train_data")
     train_loader = DataLoader(train_ds, batch_size=32, shuffle=True)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # Initialize MLP
     input_size = train_ds.X.shape[1]  # 4*7680 = 30720
+    print(f"input_size: {input_size}")
     num_classes = len(np.unique(train_ds.Y))  # should be 5
-    model = SleepStageMLP(input_dim=input_size, num_of_classes=num_classes, dropout=0.5).to(device)
+    model = SleepStageMLP_Tanh(input_dim=input_size, num_of_classes=num_classes, dropout=0.5).to(device)
 
     # Optimizer and loss
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     criterion = torch.nn.CrossEntropyLoss()
 
     # Training
-    for epoch in range(10):
+    print("start training")
+    for epoch in range(75):
         model.train()
         total_loss = 0
 
@@ -60,7 +64,7 @@ if __name__ == "__main__":
 
             optimizer.zero_grad()
             logits = model(X)
-            loss = criterion(logits, y  )
+            loss = criterion(logits, y)
             loss.backward()
             optimizer.step()
 
